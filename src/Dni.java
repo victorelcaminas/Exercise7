57c3ad17c70b69ce9376332cbd12b8f4f0e96d18
alu10571073 <aluvid073@ieselcaminass.org>
2022-10-03 16:40:20 +0200

    first commit

---
+++ src/Dni.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Dni {
+    public static void main(String[] args) {
+        int number;
+        Scanner input = new Scanner(System.in);
+
+        char[] LETTERS = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
+                'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        {
+
+
+            System.out.println("Enter your DNI number to know your letter");
+            number = input.nextInt();
+
+            System.out.println(LETTERS[number%23]);
+
+
+        }
+
+
+    }
+}
+
+
+
+
+
